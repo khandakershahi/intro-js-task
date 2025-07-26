a57ba4690f64e4feace3b5aa5282d7aadda72044
khandakershahi <khandakershahi@gmail.com>
2025-07-26 23:36:17 +0600

    task done

---
+++ task-01.js
@@ -0,0 +1,10 @@
+var totalMoney = 1000;
+var applePrice = 350;
+var orangePrice = 400;
+
+var moneyReturn = totalMoney - (applePrice + orangePrice);
+
+console.log("We have total ৳" + totalMoney );
+console.log("Apple price is ৳" + applePrice);
+console.log("Ornage price is ৳" + orangePrice);
+console.log("Shopkeeper return ৳" + moneyReturn);
