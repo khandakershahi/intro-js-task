73fe3448af6d419ff55a4d3a09d6da9412361147
khandakershahi <khandakershahi@gmail.com>
2025-07-27 17:18:37 +0600

    task two done

---
+++ task-02.js
@@ -0,0 +1,16 @@
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var avarage = (math + biology + chemistry + physics + bangla)/5;
+
+
+console.log("Mathematics number is " + math);
+console.log("Biology number is " + biology);
+console.log("Chemistry number is " + chemistry);
+console.log("Physics number is " + physics);
+console.log("Bangla number is " + bangla);
+
+console.log("Average mark is " + avarage.toFixed(2));
