acfb946573fd8c2510fb2d3315eee37025ab3f7a
khandakershahi <khandakershahi@gmail.com>
2025-07-27 17:23:33 +0600

    task 3 is done

---
+++ task-03.js
@@ -0,0 +1,9 @@
+var divider = 5;
+var dividend = 119;
+
+var reminder = 119 % 5;
+
+
+console.log("Divider is " + divider);
+console.log("Dividend is " + dividend);
+console.log("Remainder is " + reminder);
