378b4e4057a6f20b1c0e507cfe03ed37d9d829e4
khandakershahi <khandakershahi@gmail.com>
2025-07-27 17:29:55 +0600

    task 4 done

---
+++ task-04.js
@@ -0,0 +1,13 @@
+var a = isNaN('11');
+
+
+console.log(a);
+console.log(typeof a);
+
+
+var a = isNaN(2-10);
+console.log(a);
+console.log(typeof a);
+
+
+console.log("Both are number so the isNan return false.");
